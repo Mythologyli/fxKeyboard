e2c50cb352b9d0ccf5e4329937bd916b595eb7a0
lxbpxylps@126.com <lxbpxylps@126.com>
2021-04-09 23:29:06 +0800

    Make a test edition, able to send 0~9

--- KeyboardAddin/main.cpp
+++ KeyboardAddin/main.cpp
@@ -7,27 +7,48 @@ extern "C"
 #include <fxlib.h>
 
 #include "log.h"
+#include "usb.h"
+#include "sendkey.h"
 #include "syscall.h"
 
 
 Log log;
+USB usb;
+SendKey sendkey;
+
+
+// Quit handler to close the port.
+void QuitHandler(void);
 
 
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
-    unsigned int key;
-
-    log.Init();
+    log.Init(); // init log
 
     log.PrintLn((const unsigned char*)"fxKeyboard v0.1");
     log.PrintLn((const unsigned char*)"Copyright 2021 Myth");
 
-    while(1)
-    {
-        GetKey(&key);
-    }
+    usb.Init(); // open port
+    SetQuitHandler(QuitHandler); // set quit handler
+
+    sendkey.Send();
 
     return 1;
+}
+
+
+// Quit handler to close the port.
+void QuitHandler(void)
+{
+    unsigned int key;
+
+    usb.Close(); // close the port before quit
+
+    PopUpWin(1);
+    locate(4, 4);
+    Print((const unsigned char*)"Close USB Port!");
+
+    GetKey(&key); // wait for any key pressed
 }
 
 
