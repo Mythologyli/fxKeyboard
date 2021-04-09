4be5d32977404029a0e4b3543d2bc4bf19a2c74c
lxbpxylps@126.com <lxbpxylps@126.com>
2021-04-09 23:27:03 +0800

    Add file tool and uStrLen function.

---
+++ KeyboardAddin/tool.cpp
@@ -0,0 +1,44 @@
+/*
+File: tool.cpp
+Author: Myth
+*/
+
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+
+#include "tool.h"
+
+
+// Length of unsigned str.
+// Return
+// -1 : input NULL
+// -2 : no \0 in 256 bytes
+int uStrLen(const unsigned char* ustr)
+{
+    if (ustr == 0)
+        return -1;
+    
+    int i;
+    
+    for (i = 0; ; i++)
+    {
+        if (ustr[i] == '\0')
+            break;
+        else if (i > 256)
+        {
+            i = -2;
+            break;
+        }
+    }
+
+    return i;
+}
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ KeyboardAddin/tool.h
@@ -0,0 +1,25 @@
+/*
+File: tool.h 
+Author: Myth
+Usage: Some useful functions and defines.
+*/
+
+
+#ifndef _TOOL_H_
+#define _TOOL_H_
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+
+// Length of unsigned str.
+int uStrLen(const unsigned char* ustr);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
